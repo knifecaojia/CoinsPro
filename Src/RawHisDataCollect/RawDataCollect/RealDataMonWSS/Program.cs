6ee29996b3b9864d65ebafbf60d5391449c45d45
DESKTOP-FDPD7MC\knifeandcj <knifeandcj@gmail.com>
2017-10-12 16:16:21 +0800

    实时数据获取 通信成功

--- Src/RawHisDataCollect/RawDataCollect/RealDataMonWSS/Program.cs
+++ Src/RawHisDataCollect/RawDataCollect/RealDataMonWSS/Program.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using SuperSocket.ClientEngine;
 using WebSocket4Net;
+using Quobject.SocketIoClientDotNet.Client;
+using Newtonsoft.Json.Linq;
 
 namespace RealDataMonWSS
 {
@@ -14,17 +16,45 @@ namespace RealDataMonWSS
         static WebSocket Client;
         static void Main(string[] args)
         {
-            Client = new WebSocket("https://streamer.cryptocompare.com");
-            Client.Opened += new EventHandler(Client_Opened);
-            Client.Error += new EventHandler<SuperSocket.ClientEngine.ErrorEventArgs>(Client_Error);
-            Client.Closed += new EventHandler(Client_Closed);
-            Client.MessageReceived += new EventHandler<MessageReceivedEventArgs>(Client_MessageReceived);
-            Client.DataReceived += new EventHandler<DataReceivedEventArgs>(Client_DataReceived);
-            Client.Open();
-            
-            Console.ReadKey();
-        }
+            //Client = new WebSocket("ws://streamer.cryptocompare.com");
+            //Client.Opened += new EventHandler(Client_Opened);
+            //Client.Error += new EventHandler<SuperSocket.ClientEngine.ErrorEventArgs>(Client_Error);
+            //Client.Closed += new EventHandler(Client_Closed);
+            //Client.MessageReceived += new EventHandler<MessageReceivedEventArgs>(Client_MessageReceived);
+            //Client.DataReceived += new EventHandler<DataReceivedEventArgs>(Client_DataReceived);
+            //Client.Open();
+            JObject jo = JObject.Parse("{subs:\"['5~CCCAGG~BTC~USD','5~CCCAGG~ETH~USD']\"}");
+            var socket = IO.Socket("wss://streamer.cryptocompare.com");
+            socket.On(Socket.EVENT_CONNECT, () =>
+            {
 
+                Console.WriteLine("Socket.EVENT_CONNECT" );
+                JArray ja = new JArray();
+                ja.Add("2~Bitstamp~BTC~USD"); // aggregate BTC
+                ja.Add("2~Poloniex~ETH~USD"); // aggregate ETH
+                ja.Add("2~Poloniex~LTC~USD"); // aggregate ETH
+                JObject obj = new JObject();
+                try
+                {
+                    obj.Add("subs", ja);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                socket.Emit("SubAdd", obj);
+            });
+          
+            socket.On("m", (data) =>
+            {
+                Console.WriteLine(data);
+                //Console.WriteLine(System.Text.Encoding.Default.GetString((byte[])data));
+                //socket.Disconnect();
+            });
+            Console.ReadKey();
+            socket.Disconnect();
+        }
+     
         private static void Client_DataReceived(object sender, DataReceivedEventArgs e)
         {
             Console.WriteLine("Client_DataReceived"+e.Data.ToString());
