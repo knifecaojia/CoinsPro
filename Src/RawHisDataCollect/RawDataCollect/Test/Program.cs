38029aa5567a8b15676b910843997f6646f2d8a8
DESKTOP-FDPD7MC\knifeandcj <knifeandcj@gmail.com>
2017-10-19 17:10:36 +0800

    no message

---
+++ Src/RawHisDataCollect/RawDataCollect/Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using KFSoft.CommonLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpResult hr = new HttpResult();
+            HttpHelper hh = new HttpHelper();
+            HttpItem hi = new HttpItem();
+            string url = @"https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit=60&e=Bitstamp&toTs=" + TimeHelper.GetTimeStamp(DateTime.Now); ;
+            url = @"http://2017.ip138.com/ic.asp";
+            hi.URL = url;
+            hi.ProxyIp = "127.0.0.1:1080";
+            hr = hh.GetHtml(hi);
+            string rawjson = hr.Html;
+            Console.WriteLine(rawjson);
+            Console.ReadKey();
+        }
+    }
+}
